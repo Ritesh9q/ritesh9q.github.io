0113efddc81ed9b84e5365dac2966fa2bf6b74b2
Ritesh Kurve <riteshkurve9q@gmail.com>
2021-02-17 13:54:42 +0530

    New commits
    Portfolio

---
+++ js/chunk-2d0c0c24.48d798c4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c0c24"],{"42c3":function(t,n,e){"use strict";e.r(n);var c=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"container"},[e("h2",{staticClass:"h2"},[t._v("Writing✍️")])])}],a={},i=a,r=e("2877"),l=Object(r["a"])(i,c,s,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0c0c24.48d798c4.js.map
