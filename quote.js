7106c90ea50aa02962ee40eb6074bb88204237f3
Ritesh Kurve <riteshkurve9q@gmail.com>
2021-02-23 17:50:05 +0530

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,2 +1,10 @@
-quotes []={
+{
+"quotes":[
+    {"quote":"John", "author":"Doe"},
+    {"quote":"Anna", "author":"Smith"},
+    {"quote":"Anna", "author":"Smith"},
+   {"quote":"Anna", "author":"Smith"},
+   {"quote":"Anna", "author":"Smith"}
+   
+]
 }
